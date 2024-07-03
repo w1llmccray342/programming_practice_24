9453a0e918d89f8e69cb5050918f5bc959d68a61
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-03 20:50:06 +0000

    fix test_cases

--- Tests/test_cards.py
+++ Tests/test_cards.py
@@ -8,6 +8,6 @@ class Test_Config(unittest.TestCase):
         draw_card_res_2 = draw_card(1)
         draw_card_res_3 = draw_card(-1)
 
-        self.assertEqual(True, draw_card(2) == draw_card_res)
-        self.assertEqual(True, draw_card(1) == draw_card_res_2)
-        self.assertEqual(False, draw_card(-1) == draw_card_res_3)
+        self.assertEqual(True, draw_card(2) == type(list))
+        self.assertEqual(True, draw_card(1) == type(list))
+        self.assertEqual(False, draw_card(-1) == type(list))
