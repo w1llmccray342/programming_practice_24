a410af0b69818980eac465d35f7810809be97f4d
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-04 18:46:49 +0000

    Deck.py fixes

--- Games/deck.py
+++ Games/deck.py
@@ -55,13 +55,17 @@ class Deck:
                 my_draw = random.randint(0, 3)
                 my_second_draw = random.randint(0, 12)
 
-            # Add drawn cards to drawn cards list
+                 # Add drawn cards to drawn cards list
                 drawn_cards.append(deck[my_draw][my_second_draw])   
 
             # Remove drawn cards from list.
                 drawn_cards.remove(deck[my_draw][my_second_draw])
 
+                print(drawn_cards)
+                print(deck[my_draw][my_second_draw]) #Should print out nothing
 
-            print(drawn_cards)
-            print(deck[my_draw][my_second_draw]) #Should print out nothing
+           
+
+
+            
             return drawn_cards, deck
