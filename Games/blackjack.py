ce910c3b10877f25cb69c319bc397186850fa19b
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-04 21:41:11 +0000

    Fix an issue

--- Games/blackjack.py
+++ Games/blackjack.py
@@ -105,10 +105,10 @@ def game_loop():
 
             if first_draw:
                 player_hand = deck.draw_card(game_deck, 2)
+                print(game_deck)
                 print(type(player_hand))
-
                 print(player_hand[0:1])
-                print("Game deck has", (len(x) for x in game_deck), "cards left!")
+                
                 
                 # Breaks at this point but what is the reason for it?
                 # List index is out of range
