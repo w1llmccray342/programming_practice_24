1ae0905ef45072933ea79e6ad7891bc134dbf164
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-04 18:21:29 +0000

    Changed imports for Deck.

--- Tests/test_cards.py
+++ Tests/test_cards.py
@@ -1,13 +1,13 @@
 import unittest
-from Games.deck import draw_card
+from Games.deck import Deck
 
 class Test_Config(unittest.TestCase):
 
     def test_draw_card(self):
-        draw_card_res = draw_card(2)
-        draw_card_res_2 = draw_card(1)
-        draw_card_res_3 = draw_card(-1)
+        draw_card_res = Deck.draw_card(2)
+        draw_card_res_2 = Deck.draw_card(1)
+        draw_card_res_3 = Deck.draw_card(-1)
 
-        self.assertEqual(True, draw_card(2) == type(list))
-        self.assertEqual(True, draw_card(1) == type(list))
-        self.assertEqual(False, draw_card(-1) == type(list))
+        self.assertEqual(True, Deck.draw_card(2) == type(list))
+        self.assertEqual(True, Deck.draw_card(1) == type(list))
+        self.assertEqual(False, Deck.draw_card(-1) == type(list))
