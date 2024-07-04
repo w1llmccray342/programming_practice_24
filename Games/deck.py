b01704c04723f775cc7af3d98114e03f374966b0
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-04 18:43:49 +0000

    Deck.py fixes

--- Games/deck.py
+++ Games/deck.py
@@ -4,23 +4,21 @@ import random
 class Deck: 
     def __init__(self) -> None:
         pass
-   
+
+  
     def create_deck():
         default_deck = []
-    
 
         default_suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
         default_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
         for suit in default_suits:
          default_list = []
-
+         
          for value in default_values:
-            temp_value = (f"{value} Of {suit}")
-            default_list.append(temp_value)
-                
-
-        default_deck.append(default_list)    
+             temp_value = (f"{value} Of {suit}")
+             default_list.append(temp_value)
+         default_deck.append(default_list)    
 
         print(default_deck)
         return default_deck
