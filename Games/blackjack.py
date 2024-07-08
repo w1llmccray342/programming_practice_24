2209941e8ed89b1bfc88ddea3fc636d3135f000d
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 22:11:02 +0000

    Fix blackjack

--- Games/blackjack.py
+++ Games/blackjack.py
@@ -163,7 +163,8 @@ def player_options_handler(deck, score_ai, score_human, ai_hand, player_hand, ch
             # Overwrite player_hand
             ow_list = hit_fn(deck, player_hand)  
             ow_list = list(ow_list)
-            player_hand = ow_list[0]
+            my_hand = str(ow_list[0]) 
+            player_hand.append(my_hand)
             deck = ow_list[1]
 
             score_human = find_sum_of_cards(score_human, player_hand)
