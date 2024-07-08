3a2a4d79bb513d8abd7ce0ccf89c20f26b35fdf0
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-08 21:28:13 +0000

    Fixes to handle function

--- Games/blackjack.py
+++ Games/blackjack.py
@@ -160,8 +160,8 @@ def player_options_handler(deck, score_ai, score_human, ai_hand, player_hand, ch
         # os.system("clear")
         if player_option == 1:
             print(type(player_hand), player_hand)
-            my_vars = hit_fn(deck, player_hand)
-            player_hand.append(my_vars)
+            # Overwrite player_hand
+            player_hand = hit_fn(deck, player_hand)  
             print(player_hand)
 
             score_human = find_sum_of_cards(score_human, player_hand)
