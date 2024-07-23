3a560c2cee2837041cbc669cd3d6d5a18ebdf180
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-23 04:46:26 +0000

    Change useage to be methods

--- Projects/main.py
+++ Projects/main.py
@@ -9,47 +9,47 @@ import calculate_avg
 # print(my_assign_obj, "\n", my_test_obj, "\n", my_hw_obj, "\n", my_quiz_obj)
 
 
-def generate_master_list():
-    # Create a function that makes a master list of data
+# def generate_master_list():
+#     # Create a function that makes a master list of data
 
-    # Create a master list to store objects created for quiz, hw, test, assign TYPE ASSIGNMENT objects
-    my_quiz_objs = []
-    my_assign_objs = []
-    my_test_objs = []
-    my_hw_objs = []
-    my_master_obj_list = [my_assign_objs, my_hw_objs, my_quiz_objs, my_test_objs]
-    my_master_class_list = [calculate_avg.Assignment, calculate_avg.Homework, calculate_avg.Quiz, calculate_avg.Test]
-    nums = 0
+#     # Create a master list to store objects created for quiz, hw, test, assign TYPE ASSIGNMENT objects
+#     my_quiz_objs = []
+#     my_assign_objs = []
+#     my_test_objs = []
+#     my_hw_objs = []
+#     my_master_obj_list = [my_assign_objs, my_hw_objs, my_quiz_objs, my_test_objs]
+#     my_master_class_list = [calculate_avg.Assignment, calculate_avg.Homework, calculate_avg.Quiz, calculate_avg.Test]
+#     nums = 0
 
-    # Gather input from the user:
-    for assignment_type in my_master_obj_list:
+#     # Gather input from the user:
+#     for assignment_type in my_master_obj_list:
         
-        if assignment_type == my_assign_objs:
-            my_name = "Assignment"
-            my_weight = 0.1
+#         if assignment_type == my_assign_objs:
+#             my_name = "Assignment"
+#             my_weight = 0.1
 
-        elif assignment_type == my_hw_objs:
-            my_name = "HW"
-            my_weight = 0.2
+#         elif assignment_type == my_hw_objs:
+#             my_name = "HW"
+#             my_weight = 0.2
 
-        elif assignment_type == my_quiz_objs:
-            my_name = "Quiz"
-            my_weight = 0.3
+#         elif assignment_type == my_quiz_objs:
+#             my_name = "Quiz"
+#             my_weight = 0.3
 
-        elif assignment_type == my_test_objs:
-            my_name = "Test"
-            my_weight = 0.4
+#         elif assignment_type == my_test_objs:
+#             my_name = "Test"
+#             my_weight = 0.4
 
-        nums_to_add = int(input(f"Please type the number of {my_name} objects you would like to create: \n"))
+#         nums_to_add = int(input(f"Please type the number of {my_name} objects you would like to create: \n"))
 
-        for i in range(nums_to_add):
-            my_score = float(input(f"Please enter the score for {my_name} #{i+1}: \n"))
-            my_obj_to_add = my_master_class_list[nums](my_name, my_weight, my_score)
-            assignment_type.append(my_obj_to_add)
+#         for i in range(nums_to_add):
+#             my_score = float(input(f"Please enter the score for {my_name} #{i+1}: \n"))
+#             my_obj_to_add = my_master_class_list[nums](my_name, my_weight, my_score)
+#             assignment_type.append(my_obj_to_add)
         
-        nums += 1
+#         nums += 1
     
-    return my_master_obj_list
+#     return my_master_obj_list
 
 def display_master_list(obj_list):
     i = 1
@@ -58,23 +58,24 @@ def display_master_list(obj_list):
             print(f"OBJ#{i} of type {object}")
             i += 1
 
-def calculate_average(obj_list):
-    sum = 0
+# def calculate_average(obj_list):
+#     sum = 0
     
-    for object_type in obj_list:
+#     for object_type in obj_list:
         
-        for object in object_type:
-            my_obj_weight = object.return_obj_weight()
-            sum += object.score * my_obj_weight
+#         for object in object_type:
+#             my_obj_weight = object.return_obj_weight()
+#             sum += object.score * my_obj_weight
 
         
-    weighted_avg = sum
-    print(f"The current average for the class is... {weighted_avg}%")
+#     weighted_avg = sum
+#     print(f"The current average for the class is... {weighted_avg}%")
     
-    return weighted_avg
+#     return weighted_avg
 
+my_class = calculate_avg.Assignment("Test", "Example", "NIL")
 
-my_master_list = calculate_avg.Assignment.generate_master_list()
+my_master_list = calculate_avg.Assignment.generate_master_list(my_class)
 display_master_list(my_master_list)
-calculate_average(my_master_list)
+calculate_avg.Assignment.calculate_average(my_master_list)
 
