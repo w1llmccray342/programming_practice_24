ade84964512b4d6199a5dcaf2d8301dd82101321
w1llmccray342 <william.mccray@g.austincc.edu>
2024-07-23 03:16:21 +0000

    Fix calculate average and main.py

--- Projects/calculate_avg.py
+++ Projects/calculate_avg.py
@@ -1,10 +1,30 @@
 class  Assignment:
 
-    def __init__(self, name, weight):
-        .__weight = weight
-        .__name = name
+    def __init__(self, name, weight, score):
+        self.__weight = weight
+        self.__name = name
+        self.score = score
+
 
     def __str__(self):
-        return "{.__name} holds {.__weight}"
+        return f"{self.__name} holds {self.__weight} weight with a score of {self.score}"
     
+class Average:
     
+    def __init__(self):
+        pass
+
+    def grab_average(self, nums_list):
+        totals = 0
+        total_nums = 0
+
+        for num in nums_list:
+            totals += num
+            total_nums += 1
+
+        avg = totals / total_nums
+
+        return avg
+
+
+
--- Projects/main.py
+++ Projects/main.py
@@ -1,2 +1,8 @@
-from Projects.calculate_avg import Assignment
+from calculate_avg import Assignment
 
+my_quiz_obj = Assignment("Quiz", 0.3)
+my_hw_obj = Assignment("Homework", 0.2)
+my_test_obj = Assignment("Test", 0.4)
+my_assign_obj = Assignment("Assignment", 0.1)
+
+print(my_assign_obj, my_test_obj, my_hw_obj, my_quiz_obj)
